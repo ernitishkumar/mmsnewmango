dad6f5dde377a397272474369bafe55ef3ed5498
Nitish Kumar <erkumarnitish@gmail.com>
2016-01-06 18:35:34 +0530

    Production code v1.1

--- WEB-INF/classes/mms/mongo/utility/Driver.java
+++
@@ -1,7 +0,0 @@
-import mms.mongo.utility.DatabaseConnection;
-public class Driver{
-public static void main(String gg[]){
-DatabaseConnection dc=new DatabaseConnection();
-DatabaseConnection.getConnection();
-}
-}
